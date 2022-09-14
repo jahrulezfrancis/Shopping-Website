344fd5f8c14f0830153a3cdfedc46bcdcff3ff94
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-14 12:20:51 +0100

    added route configuration for homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './Components/Pages/HomePage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-    
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Box>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
