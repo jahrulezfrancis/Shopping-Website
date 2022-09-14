233f44b2a2a224c21dec34ea05b2d342683492bf
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-14 15:10:13 +0100

    updated code base

--- src/Components/Header/Navigation.jsx
+++ src/Components/Header/Navigation.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Box, Button, HStack, Image, ListItem, UnorderedList } from '@chakra-ui/react';
+import { Box, Button, Heading, HStack, Image, ListItem, Stack, UnorderedList } from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
 import {
-    Menu, MenuButton, MenuList, MenuItem, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider,
+    Menu, MenuButton, MenuList, MenuItem, IconButton, MenuItemOption, MenuGroup, MenuOptionGroup, MenuDivider,
 } from '@chakra-ui/react'
 import HeaderLogo from "../../Components/Images/Header-logo.png"
 import TextBG from "../../Components/Images/yellow-background.png"
+import { MdDashboard } from 'react-icons/md';
+
+
 
 export default function Navigation() {
     return (
-
         <Box>
             <UnorderedList textTransform='uppercase' listStyleType='none' p='1em'
                 display={{ base: 'none', md: 'none', lg: 'block' }}>
@@ -36,6 +38,57 @@ export default function Navigation() {
                     </HStack>
                 </HStack>
             </UnorderedList>
+        </Box>
+    )
+}
+
+
+
+export function MobileMenu() {
+    return (
+        <Box display={{ sm: 'block', md: 'block', lg: 'none' }} w='100%'>
+            <HStack w='100%'>
+                <Menu>
+                    <MenuButton
+                        as={IconButton}
+                        aria-label='Options'
+                        icon={<MdDashboard />}
+                        variant='outline'
+                    />
+                    <MenuList>
+                        <MenuItem>
+                            <NavLink>
+                                Catalogue
+                            </NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>
+                                Fashion
+                            </NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>
+                                Favorite
+                            </NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>
+                                LifeStyle
+                            </NavLink>
+                        </MenuItem>
+                        <MenuItem>
+                            <NavLink>
+                                <Button bgColor='black' color='white' _hover={{bgColor:'black'}}>Sign Up</Button>
+                            </NavLink>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+                <Stack align='end' justify='center' w='100%' p='.5em'>
+                    <NavLink>
+                        <Image src={HeaderLogo} alt='header logo' />
+                    </NavLink>
+                </Stack>
+            </HStack>
         </Box>
     )
 }
--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import { Box, Heading } from '@chakra-ui/react'
 import React from 'react'
-import Navigation from '../Header/Navigation'
+import IntroSection from '../Header/IntroSection'
+import Navigation, { MobileMenu } from '../Header/Navigation'
 
 
 export default function HomePage() {
     return (
         <Box>
            <Navigation />
+           <MobileMenu />
+           <IntroSection />
         </Box>
     )
 }
